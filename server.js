5d4de906119af09113bc60aa912f8f2313a88d71
kiranhsgithub <hskiran.bms@gmail.com>
2016-12-19 15:22:15 -0500

    added http request handler

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-import https from 'https';
+import http from 'http';
 
-https.get('https://www.lynda.com', res => {
-    console.log('Response status code', res.statusCode);
-    
-    res.on('data', chunk => {
-       console.log(chunk.toString()); 
-    });
+const server = http.createServer((req, res) => {
+    res.write('Hello HTTP!\n');
+    setTimeout(() => {
+       res.write('I can stream!\n'); 
+        res.end();
+    }, 3000);
 });
+
+server.listen(8082);
