ecefa1492bde2cbfb5ef4675fc61803473b773b5
kiranhsgithub <hskiran.bms@gmail.com>
2016-12-20 08:38:29 -0500

    extending react components

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Header from './Header'
 
-const App = () => {
-    return (
-        <div className="App">
-            <Header message="Naming Contests" />
-            <div>
-               ...
-            </div>
-        </div>        
-    );
+class App extends React.Component {
+    render() {
+         return (
+            <div className="App">
+                <Header message="Naming Contests" />
+                <div>
+                   ----
+                </div>
+            </div>        
+        );   
+    }
 };
 
 export default App;
