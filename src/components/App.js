f30f98299910483a0988cea4ab2d547d2c1e71e8
kiranhsgithub <hskiran.bms@gmail.com>
2016-12-20 21:45:56 -0500

    new component contest list

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import axios from 'axios';
 import Header from './Header';
-import ContestPreview from './ContestPreview';
+import ContestList from './ContestList';
 
 class App extends React.Component {
     
@@ -19,18 +18,12 @@ class App extends React.Component {
     componentWillUnmount() {
         //clean timers, listeners
     }
-
-    
     
     render() {
          return (
             <div className="App">
                 <Header message={this.state.pageHeader} />
-                <div>
-                {this.state.contests.map(contest => 
-                    <ContestPreview key={contest.id} {...contest} />
-                )}
-                </div>
+                <ContestList contests = {this.state.contests} /> 
             </div>        
         );   
     }
