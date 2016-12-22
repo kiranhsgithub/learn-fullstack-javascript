2d1d4e2870ef2f04fab89575480802abadde3f2d
kiranhsgithub <hskiran.bms@gmail.com>
2016-12-21 20:25:50 -0500

    after initial integration mongodb

--- api/index.js
+++ api/index.js
@@ -1,25 +1,34 @@
 import express from 'express';
-import data from '../src/testData';
+import { MongoClient } from 'mongodb';
+import assert from 'assert';
+import config from '../config';
+
+let mdb;
+MongoClient.connect(config.mongodbUri, (err, db) => {
+    assert.equal(null,err);
+    
+    mdb = db;
+});
 
 const router = express.Router();
-const contests = data.contests.reduce((obj, contest) => {
-        obj[contest.id] = contest;
-        return obj;
-    }, {});
 
 router.get('/contests', (req, res) => {
-    res.send({
-        contests: contests 
+    let contests = {};
+    mdb.collection('contests').find({})
+        .each((err, contest) => {
+            assert.equal(null, err);
+        
+            if(!contest) {
+                res.send(contests);
+                return;
+            }
+        
+            contests[contest.id] = contest;    
     });
 });
 
 router.get('/contests/:contestId', (req, res) => {
-    //req.params.contestId
-    let contest = contests[req.params.contestId];
-    
-    contest.description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ullamcorper elementum tellus ut dapibus. Sed nulla erat, auctor convallis cursus in, posuere ac lectus. Maecenas consequat urna id venenatis finibus. Suspendisse gravida ullamcorper dapibus. Mauris nec pharetra mauris. Etiam at tempus lorem, quis venenatis urna. Sed ante est, dapibus et gravida eu, porta pulvinar neque. Suspendisse potenti. Fusce eleifend leo vel nisi elementum aliquam. Duis sit amet ante efficitur, commodo nisi in, tempus velit. Maecenas porttitor efficitur nisl, ac convallis arcu finibus vitae. Cras sed leo nunc. Integer gravida felis augue, facilisis egestas mauris convallis aliquam. Nulla dictum magna id cursus aliquet.";
-    
-    res.send(contest);
+   
 });
 
 export default router;
--- config.js
+++ config.js
@@ -9,6 +9,7 @@ export const logStars = function(message) {
 };
 
 export default {
+    mongodbUri: 'mongodb://localhost:27017/test',
     port: env.PORT || 8082,
     host: env.HOST || 'localhost',
     get serverUrl() {
